524e8760a3ed3271b3a464860f191afc465ea569
U-MAIN\e080099 <e080099@MJ15XNP.MAIN.AD.TMH.ORG>
2018-06-06 15:44:35 -0400

    foobar

--- navscript.js
+++ navscript.js
@@ -2,8 +2,13 @@ document.getElementById("mainnav").innerHTML =
 '<ul>'+
  '<li id="ahome"><a href="index.html">Home</a></li>'+
  '<li id="aserver"><a href="servers.html">Server Info</a></li>'+
- '<li id="adocs"><a href="documentation.html">Documentation</a></li>'+
- '<li id="adocs"><a href="troubleshoot.html">Troubleshooting</a></li>'+
  '<li id="ainterface"><a href="systems.html">Interfaced Systems</a></li>'+
+ '<li id="adocs"><a href="documentation.html">Documentation</a></li>'+
+ '<li id="alinks"><a href="specs.html">Specifications</a></li>'+
+ '<li id="adocs"><a href="troubleshoot.html">Troubleshooting</a></li>'+
+ '<li id="alinks"><a href="cerner.html">Cerner</a></li>'+
+ '<li id="alinks"><a href="powershell.html">Powershell</a></li>'+
+ '<li id="alinks"><a href="visualstudio.html">Visual Studio</a></li>'+
+ '<li id="alinks"><a href="python.html">Python</a></li>'+
  '<li id="alinks"><a href="links.html">Links</a></li>'+
-'</ul>';
+'</ul>'
---
+++ scripts/footer.js
@@ -0,0 +1,4 @@
+$(function(){
+    var now = new Date();
+    $("#thefoot").append("<p>&copy; " + now.getFullYear() + " Tallahassee Memorial Healthcare (<a href=\"https://www.tmh.org/\">TMH</a>)</p>");
+});
